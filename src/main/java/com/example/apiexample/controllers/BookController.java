256b9ef15a4eb64f26391c4f1c8d4480e88f569c
matheusrgg <matheusrgg@gmail.com>
2024-02-29 14:41:11 -0300

    get funcionando

--- src/main/java/com/example/apiexample/controllers/BookController.java
+++ src/main/java/com/example/apiexample/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.example.apiexample.controllers;
 import com.example.apiexample.domain.Book;
 import com.example.apiexample.feign.BookClient;
 import com.example.apiexample.services.BookService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +23,10 @@ public class BookController {
 
 
     @GetMapping()
-    public String getBooks(){
+    public ResponseEntity<List<Book>> getBooks(){
 
         List<Book> books = this.service.getBooksFromLibrary();
-        return "oi";
+        System.out.println(books);
+        return ResponseEntity.ok().body(books);
     }
 }
